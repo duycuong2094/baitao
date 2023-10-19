be5252abbdf1ae38ba668e7f3be8acd34839a892
duycuong2094 <djkhuong1994@gmail.com>
2023-10-19 10:01:18 +0700

    update

---
+++ src/BaiTap/Bai1.java
@@ -0,0 +1,12 @@
+package BaiTap;
+
+public class Bai1 {
+    public static void main(String[] args) {
+        int[] arr={5,9,2,4,7,3,96,36,8};
+        Sort sort = new Sort();
+        sort.Sorts(arr);
+for (int i = 0 ; i<arr.length;i++){
+    System.out.println(arr[i]);
+}
+    }
+}
---
+++ src/BaiTap/Sort.java
@@ -0,0 +1,18 @@
+package BaiTap;
+
+ class Sort  extends Bai1{
+    int [] arr;
+    public void Sorts(int [] arr){
+        for (int i = 0;i<arr.length-1;i++){
+            for (int j =i+1;j<arr.length;j++){
+                if(arr[j]<arr[i]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+
+                }
+            }
+        }
+
+    }
+}
